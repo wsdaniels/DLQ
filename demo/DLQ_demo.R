343c07d7a75084fd972300147f810266742b4727
William Daniels <wdaniels@sentinel.local>
2025-05-27 08:35:43 -0600

    updated demo

--- demo/DLQ_demo.R
+++ demo/DLQ_demo.R
@@ -25,7 +25,6 @@ if (commandArgs()[1] == "RStudio"){
 # Set parameter values
 gap.time           <- 30
 length.threshold   <- 15
-do.event.detection <- F
 
 # Get directories
 simulation.directory <- "./input_files/simulation_files/"
@@ -46,8 +45,10 @@ source('https://raw.github.com/wsdaniels/DLQ/master/code/HELPER_spike_detection_
 # STEP 1: READ IN CONFIG FILE AND SET UP PARAMETERS AND DIRECTORY STRUCTURE
 #---------------------------------------------------------------------------
 
+# Get the simulation files
 sim.files <- list.files(simulation.directory)
 
+# Combine all simulation files into a list
 sims <- vector(mode = "list", length = length(sim.files))
 names(sims) <- sapply(strsplit(sim.files, "\\."), function(X) X[[1]])
 for (i in 1:length(sim.files)){
@@ -56,29 +57,16 @@ for (i in 1:length(sim.files)){
   sims[[i]] <- this.sim[,-1]
 }
 
-obs <- read.csv(paste0(observation.file))[,-1]
-
-times <- as_datetime(times)
-times <- as_datetime(times, tz = "America/Denver")
-
-data <- list(times = times, obs = obs)
-data <- c(data, sims)
-
-# Read in simulation data
-# data <- readRDS(simulation.data.path)
-
-# Pull out sensor observations and replace NA's that are not on edge of 
-# the time series with interpolated values
-obs <- na.approx(data$obs, na.rm = F)
+# Get observations
+obs <- read.csv(paste0(observation.file))[,-c(1)]
 
 # Number of sensors
 n.r <- ncol(obs)
 
-# Pull out time stamps of observations and simulations
-times <- data$times
+# Convert times to a datetime object
+times <- as_datetime(times)
+times <- as_datetime(times, tz = "America/Denver")
 
-# Pull out the simulation predictions
-sims <- data[3:length(data)]
 
 # Grab source info
 n.s <- length(sims) 
@@ -118,7 +106,7 @@ for (j in to.use){
                         make.plot = F)
   
   # Add points immediately before and after the spike to the spike mask
-  # This better captures the full event, as there is some delay between the 
+  # This better captures the full event, as there is some delay between the
   # start of an emission and when the sensors first see an enhancement
   for (i in na.omit(unique(spikes$events))){
     min.ind <- min(which(spikes$events == i))
@@ -165,8 +153,8 @@ for (j in to.use){
   if (length(event.nums) > 0){
     
     # Loop through events and (1) fill in any gaps between events with the correct
-    # event number, and (2) estimate background as mean of first and last spike 
-    # points, which occur before the sharp increase and after the spike has 
+    # event number, and (2) estimate background as mean of first and last spike
+    # points, which occur before the sharp increase and after the spike has
     # returned to return.threshold percent of the max value
     for (i in 1:length(event.nums)){
       
@@ -178,7 +166,7 @@ for (j in to.use){
       
       # Estimate background using observations before and after spike
       # This is just the first and last observation within the spike mask,
-      # since we already added the points before and after the spike to the 
+      # since we already added the points before and after the spike to the
       # spike mask earlier on
       b.left <- trimmed.obs[first.ob]
       b.right <- trimmed.obs[last.ob]
@@ -191,11 +179,11 @@ for (j in to.use){
   }
   
   # Remove background from all non-spike data. Since by definition these points
-  # are not in an event, their concentration value is directly taken as the 
+  # are not in an event, their concentration value is directly taken as the
   # background estimate. Hence removing background is just setting the value to zero
   trimmed.obs[is.na(spikes$events)] <- 0
   
-  # Set any negative values to zero. Negative value would arise if the 
+  # Set any negative values to zero. Negative value would arise if the
   # background estimate was too large (greater than actual concentration value)
   trimmed.obs[trimmed.obs < 0] <- 0
   
@@ -205,9 +193,7 @@ for (j in to.use){
 
 
 
-
-
-# STEP 3: EVENT DETECTION
+# STEP 3: DEFINE INVERSION WINDOWS
 #---------------------------------------------------------------------------
 
 # Compute maximum concentration value across sensors for each minute
@@ -216,98 +202,19 @@ for (j in to.use){
 # (and hence no need to run the spike detection algorithm again)
 max.obs <- apply(obs, 1, max, na.rm = T)
 
-# Estimate emission start and end times 
-if (do.event.detection){
-  
-  # Create data frame with time steps and event mask
-  spikes <- data.frame(time = times, events = max.obs > 0)
-  
-  # Find gaps between events that are shorter than gap.time and turn them into events
-  #   to.replace holds the indices that need to be switched from F to T
-  #   first.gap is an indicator for the first gap (which should not be replaced,
-  #   regardless of length)
-  #   false.seq holds the indices of each sequence of FALSEs (non-events)
-  to.replace <- c()
-  first.gap <- T
-  false.seq <- c()
-  
-  # Loop through times
-  for (i in 1:length(times)){
-    
-    # If not a spike, add index to false.sequence
-    if (!spikes$events[i]){
-      false.seq <- c(false.seq, i)
-      
-      # Otherwise, check length of false sequence, if greater than gap time,
-      # save those indices to replace later
-    } else if (spikes$events[i]){
-      
-      if (length(false.seq) <= gap.time & !first.gap){
-        to.replace <- c(to.replace, false.seq)
-      }
-      
-      first.gap <- F
-      false.seq <- c()
-    }
-  }
-  
-  # Replace gaps shorter than gap.time with T (meaning they are in an event)
-  spikes$events[to.replace] <- T
-  
-  # Now we replace the T/F with an integer to distinguish between events
-  # Start by replacing F with NA and T with zero
-  spikes$events[!spikes$events] <- NA
-  spikes$events[spikes$events] <- 0
-  
-  # Get indices of spikes
-  spike.points <- which(!is.na(spikes$events))
-  count <- 0
-  
-  # Loop through spike points, if last point was not a spike, increase counter
-  for (i in spike.points){
-    
-    if (is.na(spikes$events[i-1])){
-      count <- count + 1
-      spikes$events[i] <- count
-    } else {
-      spikes$events[i] <- count
-    }
-  }
-  
-  # Get integers that uniquely define the different events
-  event.nums <- na.omit(unique(spikes$events))
-  
-  # Filter events by the length threshold
-  for (i in 1:length(event.nums)){
-    this.spike <- which(spikes$events == event.nums[i])
-    
-    if (length(this.spike) < length.threshold){
-      spikes$events[this.spike] <- NA
-    }
-  }
-  
-  # Grab event number again after filtering by length
-  event.nums <- na.omit(unique(spikes$events))
-  
-  # Number of events that we will perform localization / quantification on
-  n.ints <- length(event.nums)
-  
-  
-  # Perform localization and quantification on non-overlapping 30-minute intervals
-} else {
-  
-  # Get range of times, rounded to 30-minute intervals
-  date.range <- range(data$times)
-  date.range[1] <- ceiling_date(date.range[1], unit = "30 minutes") 
-  date.range[2] <- floor_date(date.range[2], unit = "30 minutes") 
-  
-  # Times separating the 30-minute intervals
-  int.breaks <- seq(date.range[1], date.range[2], by = "30 min") 
-  
-  # Number of intervals
-  n.ints <- length(int.breaks) - 1
-  
-}
+
+# Get range of times, rounded to 30-minute intervals
+date.range <- range(times)
+date.range[1] <- ceiling_date(date.range[1], unit = "30 minutes") 
+date.range[2] <- floor_date(date.range[2], unit = "30 minutes") 
+
+# Times separating the 30-minute intervals
+int.breaks <- seq(date.range[1], date.range[2], by = "30 min") 
+
+# Number of intervals
+n.ints <- length(int.breaks) - 1
+
+
 
 
 # STEP 4: COMPUTE ALIGNMENT METRIC
@@ -319,16 +226,8 @@ metrics <- matrix(NA, nrow = n.ints, ncol = n.s)
 # Loop through events
 for (t in 1:n.ints){
   
-  # If doing event detection, use the timing from the spike detection output
-  if (do.event.detection){
-    
-    this.mask <- seq(min(which(spikes$events == event.nums[t])),
-                     max(which(spikes$events == event.nums[t])))
-    
-    # If doing 30-minute mode, use the interval breaks defined earlier
-  } else {
-    this.mask <- spikes$time %within% interval(int.breaks[t], int.breaks[t+1])
-  }
+  # If doing 30-minute mode, use the interval breaks defined earlier
+  this.mask <- times %within% interval(int.breaks[t], int.breaks[t+1])
   
   # Loop through potential sources
   for (s in 1:n.s){
@@ -404,15 +303,7 @@ for (t in 1:n.ints){
   loc.est.all.events[t] <- source.names[which.max(these.metrics)]
   
   # If doing 30-minute mode, use the interval breaks defined earlier
-  if (do.event.detection){
-    
-    this.mask <- seq(min(which(spikes$events == event.nums[t])),
-                     max(which(spikes$events == event.nums[t])))
-    
-    # If doing 30-minute mode, use the interval breaks defined earlier
-  } else {
-    this.mask <- spikes$time %within% interval(int.breaks[t], int.breaks[t+1])
-  }
+  this.mask <- times %within% interval(int.breaks[t], int.breaks[t+1])
   
   # Get predictions from most likely source and observations during this event
   event.preds <- sims [[which.max(these.metrics)]][this.mask, ]
@@ -453,10 +344,6 @@ for (t in 1:n.ints){
   # If there is enough data to compare, compute rate estimate and 
   # percent difference between obs and preds (will be used for UQ)
   if (length(all.preds.to.compare) > 4){
-    
-    # Save obs and preds so that we can check that they are on the same order of magnitude later
-    all.preds.to.compare.all.events <- c(all.preds.to.compare.all.events, all.preds.to.compare)
-    all.obs.to.compare.all.events <- c(all.obs.to.compare.all.events, all.obs.to.compare)
     
     # Number of times to sample from predictions and observations
     n.samples <- 1000
@@ -501,10 +388,9 @@ for (t in 1:n.ints){
     } # End loop through samples
     
     # Save mean of sampled emission rates and the 5th and 95th percentiles
-    # Note: 3600 multiplier converts from g/s to g/hr
-    rate.est.all.events[t] <- mean(q.vals, na.rm = T) * 3600
-    error.lower.all.events[t] <- quantile(q.vals, probs = 0.05, na.rm = T) * 3600
-    error.upper.all.events[t] <- quantile(q.vals, probs = 0.95, na.rm = T) * 3600
+    rate.est.all.events[t] <- mean(q.vals, na.rm = T) 
+    error.lower.all.events[t] <- quantile(q.vals, probs = 0.05, na.rm = T) 
+    error.upper.all.events[t] <- quantile(q.vals, probs = 0.95, na.rm = T) 
     
     # If there are not enough time steps in which both observations and
     # predictions are in a spike, then do not estimate a rate.
@@ -530,34 +416,15 @@ for (t in 1:n.ints){
   }
 } # End loop through events
 
-# Check that predictions and observations are on the same order of magnitude
-med.p <- median(all.preds.to.compare.all.events, na.rm = T)
-med.o <- median(all.obs.to.compare.all.events,   na.rm = T)
-
-if ( med.p > med.o * 10 & med.p < med.o / 10 ){
-  if (med.p > med.o){
-    print("WARNING: Simulation output and observations are on different order, recommend re-simulating with smaller rate")
-  } else {
-    print("WARNING: Simulation output and observations are on different order, recommend re-simulating with larger rate")
-  }
-}
-
-# Package up event detection, localization, and quantification results
-# to.save <- list(event.mask = spikes,
-#                 max.obs = max.obs,
-#                 localization.estimates = loc.est.all.events,
-#                 rate.estimates = rate.est.all.events,
-#                 error.lower = error.lower.all.events,
-#                 error.upper = error.upper.all.events,
-#                 source.names = source.names,
-#                 WD = data$WD,
-#                 WS = data$WS)
-
-to.save <- data.frame(time = int.breaks[-length(int.breaks)], rate.estimate = rate.est.all.events,
+# Save output
+to.save <- data.frame(time = int.breaks[-length(int.breaks)], 
                       localization.estimate = loc.est.all.events,
+                      rate.estimate = rate.est.all.events,
                       rate.estimate.lower = error.lower.all.events,
                       rate.estimate.upper = error.upper.all.events)
 
+# Truth: tanks @ 2.1 kg/hr
+
 # Save results
-write.csv(to.save, paste0(output.file.path, "DLQ_output.csv"))
+write.csv(to.save, paste0(output.file.path, "DLQ_demo_output.csv"))
 
